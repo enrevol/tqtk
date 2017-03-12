e3e591fc5c6e207732e5ad866bed3dedd2b62909
Zinge <enrevol@gmail.com>
2017-03-13 01:09:30 +0700

    [Partial] Update auto weave view.

---
+++ k8asd/Tools/AutoWeaveView.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace k8asd {
+    partial class AutoWeaveView {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            this.playerList = new BrightIdeasSoftware.ObjectListView();
+            this.spinnerLevelColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.successRateColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.criticalRateColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.priceColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.turnColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.nameColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.cooldownColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.weaveTimer = new System.Windows.Forms.Timer(this.components);
+            this.hostInput = new System.Windows.Forms.TextBox();
+            this.autoWeave = new System.Windows.Forms.CheckBox();
+            this.cooldownLabel = new System.Windows.Forms.Label();
+            this.textileLevelInput = new System.Windows.Forms.NumericUpDown();
+            this.weaveButton = new System.Windows.Forms.Button();
+            this.autoMake = new System.Windows.Forms.CheckBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.playerList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textileLevelInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(15, 55);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(56, 13);
+            label1.TabIndex = 33;
+            label1.Text = "Chủ tổ đội";
+            // 
+            // playerList
+            // 
+            this.playerList.AllColumns.Add(this.spinnerLevelColumn);
+            this.playerList.AllColumns.Add(this.successRateColumn);
+            this.playerList.AllColumns.Add(this.criticalRateColumn);
+            this.playerList.AllColumns.Add(this.priceColumn);
+            this.playerList.AllColumns.Add(this.turnColumn);
+            this.playerList.AllColumns.Add(this.nameColumn);
+            this.playerList.AllColumns.Add(this.cooldownColumn);
+            this.playerList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.playerList.CellEditUseWholeCell = false;
+            this.playerList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.spinnerLevelColumn,
+            this.successRateColumn,
+            this.criticalRateColumn,
+            this.priceColumn,
+            this.turnColumn,
+            this.nameColumn,
+            this.cooldownColumn});
+            this.playerList.Cursor = System.Windows.Forms.Cursors.Default;
+            this.playerList.FullRowSelect = true;
+            this.playerList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.playerList.Location = new System.Drawing.Point(10, 180);
+            this.playerList.MultiSelect = false;
+            this.playerList.Name = "playerList";
+            this.playerList.SelectColumnsOnRightClick = false;
+            this.playerList.SelectColumnsOnRightClickBehaviour = BrightIdeasSoftware.ObjectListView.ColumnSelectBehaviour.None;
+            this.playerList.ShowGroups = false;
+            this.playerList.Size = new System.Drawing.Size(450, 270);
+            this.playerList.TabIndex = 31;
+            this.playerList.UseCompatibleStateImageBehavior = false;
+            this.playerList.View = System.Windows.Forms.View.Details;
+            // 
+            // spinnerLevelColumn
+            // 
+            this.spinnerLevelColumn.AspectName = "";
+            this.spinnerLevelColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.spinnerLevelColumn.MaximumWidth = 55;
+            this.spinnerLevelColumn.MinimumWidth = 55;
+            this.spinnerLevelColumn.Text = "C. Nhân";
+            this.spinnerLevelColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.spinnerLevelColumn.Width = 55;
+            // 
+            // successRateColumn
+            // 
+            this.successRateColumn.AspectName = "";
+            this.successRateColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.successRateColumn.MaximumWidth = 55;
+            this.successRateColumn.MinimumWidth = 55;
+            this.successRateColumn.Text = "T. Công";
+            this.successRateColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.successRateColumn.Width = 55;
+            // 
+            // criticalRateColumn
+            // 
+            this.criticalRateColumn.AspectName = "";
+            this.criticalRateColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.criticalRateColumn.MaximumWidth = 55;
+            this.criticalRateColumn.MinimumWidth = 55;
+            this.criticalRateColumn.Text = "B. Kích";
+            this.criticalRateColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.criticalRateColumn.Width = 55;
+            // 
+            // priceColumn
+            // 
+            this.priceColumn.MaximumWidth = 40;
+            this.priceColumn.MinimumWidth = 40;
+            this.priceColumn.Text = "Giá";
+            this.priceColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.priceColumn.Width = 40;
+            // 
+            // turnColumn
+            // 
+            this.turnColumn.MaximumWidth = 40;
+            this.turnColumn.MinimumWidth = 40;
+            this.turnColumn.Text = "Lượt";
+            this.turnColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.turnColumn.Width = 40;
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.AspectName = "";
+            this.nameColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nameColumn.MaximumWidth = 120;
+            this.nameColumn.MinimumWidth = 120;
+            this.nameColumn.Text = "Người chơi";
+            this.nameColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nameColumn.Width = 120;
+            // 
+            // cooldownColumn
+            // 
+            this.cooldownColumn.AspectName = "";
+            this.cooldownColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.cooldownColumn.MaximumWidth = 60;
+            this.cooldownColumn.MinimumWidth = 60;
+            this.cooldownColumn.Text = "Đ. băng";
+            this.cooldownColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(10, 10);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(100, 30);
+            this.refreshButton.TabIndex = 32;
+            this.refreshButton.Text = "Làm mới dệt";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // weaveTimer
+            // 
+            this.weaveTimer.Interval = 1000;
+            this.weaveTimer.Tick += new System.EventHandler(this.weaveTimer_Tick);
+            // 
+            // hostInput
+            // 
+            this.hostInput.Location = new System.Drawing.Point(85, 52);
+            this.hostInput.Name = "hostInput";
+            this.hostInput.Size = new System.Drawing.Size(80, 20);
+            this.hostInput.TabIndex = 34;
+            this.hostInput.Text = "Zinge";
+            // 
+            // autoWeave
+            // 
+            this.autoWeave.AutoSize = true;
+            this.autoWeave.Location = new System.Drawing.Point(250, 16);
+            this.autoWeave.Name = "autoWeave";
+            this.autoWeave.Size = new System.Drawing.Size(85, 17);
+            this.autoWeave.TabIndex = 35;
+            this.autoWeave.Text = "Tự động dệt";
+            this.autoWeave.UseVisualStyleBackColor = true;
+            // 
+            // cooldownLabel
+            // 
+            this.cooldownLabel.AutoSize = true;
+            this.cooldownLabel.Location = new System.Drawing.Point(350, 17);
+            this.cooldownLabel.Name = "cooldownLabel";
+            this.cooldownLabel.Size = new System.Drawing.Size(108, 13);
+            this.cooldownLabel.TabIndex = 36;
+            this.cooldownLabel.Text = "Đóng băng: 00:15:20";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(185, 55);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(43, 13);
+            label2.TabIndex = 37;
+            label2.Text = "Cấp vải";
+            // 
+            // textileLevelInput
+            // 
+            this.textileLevelInput.Location = new System.Drawing.Point(235, 53);
+            this.textileLevelInput.Maximum = new decimal(new int[] {
+            70,
+            0,
+            0,
+            0});
+            this.textileLevelInput.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.textileLevelInput.Name = "textileLevelInput";
+            this.textileLevelInput.Size = new System.Drawing.Size(50, 20);
+            this.textileLevelInput.TabIndex = 38;
+            this.textileLevelInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textileLevelInput.Value = new decimal(new int[] {
+            21,
+            0,
+            0,
+            0});
+            // 
+            // weaveButton
+            // 
+            this.weaveButton.Location = new System.Drawing.Point(120, 10);
+            this.weaveButton.Name = "weaveButton";
+            this.weaveButton.Size = new System.Drawing.Size(100, 30);
+            this.weaveButton.TabIndex = 39;
+            this.weaveButton.Text = "Dệt";
+            this.weaveButton.UseVisualStyleBackColor = true;
+            // 
+            // autoMake
+            // 
+            this.autoMake.AutoSize = true;
+            this.autoMake.Location = new System.Drawing.Point(310, 54);
+            this.autoMake.Name = "autoMake";
+            this.autoMake.Size = new System.Drawing.Size(131, 17);
+            this.autoMake.TabIndex = 40;
+            this.autoMake.Text = "Dệt chung khi lượt > 1";
+            this.autoMake.UseVisualStyleBackColor = true;
+            // 
+            // AutoWeaveView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 461);
+            this.Controls.Add(this.autoMake);
+            this.Controls.Add(this.weaveButton);
+            this.Controls.Add(this.textileLevelInput);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.cooldownLabel);
+            this.Controls.Add(this.autoWeave);
+            this.Controls.Add(this.hostInput);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.playerList);
+            this.Name = "AutoWeaveView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoWeaveView";
+            ((System.ComponentModel.ISupportInitialize)(this.playerList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textileLevelInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.ObjectListView playerList;
+        private BrightIdeasSoftware.OLVColumn spinnerLevelColumn;
+        private BrightIdeasSoftware.OLVColumn successRateColumn;
+        private BrightIdeasSoftware.OLVColumn cooldownColumn;
+        private BrightIdeasSoftware.OLVColumn criticalRateColumn;
+        private BrightIdeasSoftware.OLVColumn nameColumn;
+        private System.Windows.Forms.Button refreshButton;
+        private BrightIdeasSoftware.OLVColumn priceColumn;
+        private BrightIdeasSoftware.OLVColumn turnColumn;
+        private System.Windows.Forms.Timer weaveTimer;
+        private System.Windows.Forms.TextBox hostInput;
+        private System.Windows.Forms.CheckBox autoWeave;
+        private System.Windows.Forms.Label cooldownLabel;
+        private System.Windows.Forms.NumericUpDown textileLevelInput;
+        private System.Windows.Forms.Button weaveButton;
+        private System.Windows.Forms.CheckBox autoMake;
+    }
+}
---
+++ k8asd/Tools/AutoWeaveView.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+
+namespace k8asd {
+    public partial class AutoWeaveView : Form {
+        private int NoTeam = -1;
+
+        private enum WeaveMode {
+            /// <summary>
+            /// Dệt chung.
+            /// </summary>
+            WeaveTogether,
+
+            /// <summary>
+            /// Kéo vải.
+            /// </summary>
+            PullLevel,
+        }
+
+        private Dictionary<WeaveInfo, ClientView> mappedClients;
+        private List<WeaveInfo> infos;
+
+        private int hostingTeamId;
+        private bool isRefreshing;
+        private bool isWeaving;
+        private bool timerLocking;
+
+        public AutoWeaveView() {
+            InitializeComponent();
+
+            mappedClients = new Dictionary<WeaveInfo, ClientView>();
+            infos = new List<WeaveInfo>();
+
+            hostingTeamId = NoTeam;
+            isRefreshing = false;
+            isWeaving = false;
+            timerLocking = false;
+
+            spinnerLevelColumn.AspectGetter = obj => ((WeaveInfo) obj).Level;
+            successRateColumn.AspectGetter = obj => ((WeaveInfo) obj).SuccessRate;
+            criticalRateColumn.AspectGetter = obj => ((WeaveInfo) obj).CriticalRate;
+            priceColumn.AspectGetter = obj => {
+                var info = (WeaveInfo) obj;
+                return String.Format("{0} {1}", info.Price,
+                    (info.PriceWay == WeavePriceWay.Up ? "▲" : "▼"));
+            };
+            turnColumn.AspectGetter = obj => ((WeaveInfo) obj).Turns;
+            nameColumn.AspectGetter = obj => mappedClients[(WeaveInfo) obj].PlayerName;
+            cooldownColumn.AspectGetter = obj => Utils.FormatDuration(((WeaveInfo) obj).Cooldown);
+
+            weaveTimer.Start();
+            weaveTimer.Interval = 1000; // 1 giây.
+        }
+
+        private async void refreshButton_Click(object sender, EventArgs e) {
+            await RefreshPlayersAsync(FindConnectedClients());
+        }
+
+        private List<ClientView> FindConnectedClients() {
+            var clients = ClientManager.Instance.Clients;
+            var connectedClients = new List<ClientView>();
+            foreach (var client in clients) {
+                if (client.Connected) {
+                    connectedClients.Add(client);
+                }
+            }
+            return connectedClients;
+        }
+
+        private async Task<bool> RefreshPlayersAsync(List<ClientView> connectedClients) {
+            if (isRefreshing) {
+                return false;
+            }
+            isRefreshing = true;
+
+            mappedClients.Clear();
+            infos.Clear();
+            playerList.Items.Clear();
+
+            foreach (var client in connectedClients) {
+                var packet = await client.RefreshWeaveAsync();
+                if (packet == null) {
+                    continue;
+                }
+                Debug.Assert(packet.CommandId == "45200");
+                var token = JToken.Parse(packet.Message);
+
+                var info = WeaveInfo.Parse(token);
+                if (info == null) {
+                    continue;
+                }
+
+                mappedClients.Add(info, client);
+                infos.Add(info);
+                playerList.SetObjects(infos);
+            }
+
+            isRefreshing = false;
+            return true;
+        }
+
+        private async Task<bool> RefreshPlayerAsync(WeaveInfo weaveInfo) {
+            if (isRefreshing) {
+                return false;
+            }
+            isRefreshing = true;
+            try {
+                var index = infos.FindIndex(info => info == weaveInfo);
+                Debug.Assert(index != -1);
+
+                var client = mappedClients[weaveInfo];
+                var packet = await client.RefreshWeaveAsync();
+                if (packet == null) {
+                    return false;
+                }
+
+                var token = JToken.Parse(packet.Message);
+                infos[index] = WeaveInfo.Parse(token);
+                playerList.SetObjects(infos);
+            } finally {
+                isRefreshing = false;
+            }
+            return true;
+        }
+
+        private async Task<bool> WeaveAsync(IPacketWriter host, string hostName, WeaveMode mode,
+            IPacketWriter slot1, IPacketWriter slot2) {
+            // Lập tổ đội.
+            var textileLevel = (int) textileLevelInput.Value;
+            try {
+                host.PacketReceived += OnPacketReceived;
+                var p0 = await host.CreateWeaveAsync(textileLevel, WeaveTeamLimit.Nation);
+                if (p0 == null) {
+                    return false;
+                }
+            } finally {
+                host.PacketReceived -= OnPacketReceived;
+            }
+            // FIXME: Kiểm tra có tạo được không?
+
+            try {
+                Debug.Assert(hostingTeamId != NoTeam);
+
+                // Gia nhập tổ đội.
+                var p2s = await Task.WhenAll(slot1.JoinWeaveAsync(hostingTeamId), slot2.JoinWeaveAsync(hostingTeamId));
+                if (p2s[0] == null || p2s[1] == null) {
+                    return false;
+                }
+
+                if (mode == WeaveMode.WeaveTogether) {
+                    var p3 = await host.MakeWeaveAsync(hostingTeamId);
+                    if (p3 == null) {
+                        return false;
+                    }
+                } else {
+                    var p4 = await host.QuitAndMakeWeaveAsync(hostingTeamId);
+                    if (p4 == null) {
+                        return false;
+                    }
+                }
+                return true;
+            } finally {
+                hostingTeamId = NoTeam;
+            }
+        }
+
+        private class WeaveTeamInfo {
+            public WeaveInfo Host { get; private set; }
+            public WeaveInfo Slot1 { get; private set; }
+            public WeaveInfo Slot2 { get; private set; }
+
+            public WeaveTeamInfo(WeaveInfo host, WeaveInfo slot1) {
+                Host = host;
+                Slot1 = slot1;
+                Slot2 = null;
+            }
+
+            public WeaveTeamInfo(WeaveInfo host, WeaveInfo slot1, WeaveInfo slot2) {
+                Host = host;
+                Slot1 = slot1;
+                Slot2 = slot2;
+            }
+        }
+
+        private WeaveTeamInfo FindWeaveTeam(WeaveInfo host, List<WeaveInfo> members) {
+            if (host.Cooldown > 0) {
+                return null;
+            }
+            if (members.Count == 0) {
+                return null;
+            }
+            if (members.Count == 1) {
+                if (members[0].Cooldown > 0) {
+                    return null;
+                }
+                return new WeaveTeamInfo(host, members[0]);
+            }
+            var orderedMembers = members.OrderBy(info => info.Cooldown);
+            if (members[0].Cooldown > 0 && members[1].Cooldown > 0) {
+                return null;
+            }
+            return new WeaveTeamInfo(host, members[0], members[1]);
+        }
+
+        private async Task<bool> RecheckCooldown(WeaveInfo info) {
+            var client = mappedClients[info];
+            var ok = await RefreshPlayerAsync(info);
+            return ok;
+        }
+
+        private async Task<bool> WeaveAsync(WeaveInfo host, List<WeaveInfo> members) {
+            if (isRefreshing) {
+                return false;
+            }
+            if (isWeaving) {
+                return false;
+            }
+            isWeaving = true;
+
+            if (members.Count == 1) {
+                // Chỉ có 1 thành viên.
+                if (host.Turns > 1 && autoMake.Checked) {
+
+                } else {
+
+                }
+            }
+            var orderedMembers = members.OrderBy(info => info.Cooldown);
+
+            isWeaving = false;
+            return true;
+        }
+
+        /// <summary>
+        /// Cập nhật thời gian đóng băng.
+        /// </summary>
+        /// <returns>Thời gian đóng băng tất cả các tài khoản.</returns>
+        private void UpdateCooldown() {
+            int maxCooldown = infos.Count == 0 ? 0 : infos.Max(info => info.Cooldown);
+            cooldownLabel.Text = String.Format("Đóng băng: {0}", Utils.FormatDuration(maxCooldown));
+        }
+
+        private async void weaveTimer_Tick(object sender, EventArgs e) {
+            UpdateCooldown();
+
+            if (timerLocking) {
+                return;
+            }
+            timerLocking = true;
+            try {
+                if (!autoWeave.Checked) {
+                    return;
+                }
+
+                if (infos.Count == 0) {
+                    // Làm mới thông tin tất cả tài khoản.
+                    await RefreshPlayersAsync(FindConnectedClients());
+                    if (infos.Count == 0) {
+                        // Không có tài khoản được kết nối.
+                        autoWeave.Checked = false;
+                        return;
+                    }
+                }
+
+                if (isRefreshing || isWeaving) {
+                    return;
+                }
+
+                var hostInfo = infos.FirstOrDefault(info => mappedClients[info].PlayerName == hostInput.Text);
+                if (hostInfo == null) {
+                    // Không tìm thấy chủ tổ đội có tên được nhập.
+                    return;
+                }
+
+                var memberInfos = infos.Where(info => info != hostInfo).ToList();
+                if (memberInfos.Count == 0) {
+                    // Không có tài khoản thành viên nào.
+                    autoWeave.Checked = false;
+                    return;
+                }
+
+                int maxTurns = memberInfos.Max(info => info.Turns);
+                if (maxTurns == 0) {
+                    // Các tài khoản khác hết lượt dệt.
+                    autoWeave.Checked = false;
+                    return;
+                }
+
+                var team = FindWeaveTeam(hostInfo, memberInfos);
+                if (team == null) {
+                    return;
+                }
+
+                // Kiểm tra lại đóng băng.
+                await RefreshPlayerAsync(hostInfo);
+
+                await WeaveAsync(hostInfo, memberInfos);
+            } finally {
+                timerLocking = false;
+            }
+        }
+
+        public void OnPacketReceived(object sender, Packet packet) {
+            if (packet.CommandId == "45300") {
+                var token = JToken.Parse(packet.Message);
+                var teamId = (int) token["teamObject"]["teamid"];
+                hostingTeamId = teamId;
+            }
+        }
+    }
+}
