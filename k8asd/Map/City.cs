909150992b9a6f98427ff14d00700e02060a6c08
Zinge <enrevol@gmail.com>
2017-03-26 17:39:48 +0700

    [Auto-merchant] Update.

---
+++ k8asd/Map/City.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace k8asd {
+    /// <summary>
+    /// Thuộc gói tin 31101 và 31102.
+    /// </summary>
+    class City {
+        /// <summary>
+        /// Vị trí của thành phố này trong khu vực (1 đến 16).
+        /// </summary>
+        public int Index { get; private set; }
+
+        /// <summary>
+        /// Cấp người chơi.
+        /// </summary>
+        public int Level { get; private set; }
+
+        /// <summary>
+        /// Tên bang hội.
+        /// </summary>
+        public string LegionName { get; private set; }
+
+        /// <summary>
+        /// ID của người chơi.
+        /// </summary>
+        public int PlayerId { get; private set; }
+
+        /// <summary>
+        /// Tên người chơi.
+        /// </summary>
+        public string PlayerName { get; private set; }
+
+        /// <summary>
+        /// Quốc gia người chơi.
+        /// </summary>
+        public Nation Nation { get; private set; }
+
+        public static City Parse(JToken token) {
+            var result = new City();
+            result.Index = (int) token["index"];
+            result.Level = (int) token["citylevel"];
+            result.LegionName = (string) token["legionName"];
+            result.PlayerId = (int) token["playerid"];
+            result.PlayerName = (string) token["playername"];
+            result.Nation = (Nation) (int) token["nation"];
+            return result;
+        }
+    }
+}
