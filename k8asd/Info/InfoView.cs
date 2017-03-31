39e41df685e5a7691f871833ea04b35f1f766b97
Zinge <enrevol@gmail.com>
2017-03-31 22:02:45 +0700

    Add ID label.

--- k8asd/Info/InfoView.cs
+++ k8asd/Info/InfoView.cs
@@ -34,17 +34,17 @@ namespace k8asd {
         }
 
         private void OnPlayerIdChanged(object sender, int playerId) {
-            infoBox.Text = String.Format("{0} Lv. {1} - {2} - {3}",
+            infoBox.Text = String.Format("{0} Lv. {1} - ID: {2} - {3}",
                 model.PlayerName, model.PlayerLevel, playerId, Utils.FormatDuration(model.ServerTime));
         }
 
         private void OnPlayerNameChanged(object sender, string playerName) {
-            infoBox.Text = String.Format("{0} Lv. {1} - {2} - {3}",
+            infoBox.Text = String.Format("{0} Lv. {1} - ID: {2} - {3}",
                 playerName, model.PlayerLevel, model.PlayerId, Utils.FormatDuration(model.ServerTime));
         }
 
         private void OnPlayerLevelChanged(object sender, int playerLevel) {
-            infoBox.Text = String.Format("{0} Lv. {1} - {2} - {0}",
+            infoBox.Text = String.Format("{0} Lv. {1} - ID: {2} - {0}",
                 model.PlayerName, playerLevel, model.PlayerId, Utils.FormatDuration(model.ServerTime));
         }
 
@@ -85,7 +85,7 @@ namespace k8asd {
         }
 
         private void serverTimer_Tick(object sender, EventArgs e) {
-            infoBox.Text = String.Format("{0} Lv. {1} - {2} - {3}",
+            infoBox.Text = String.Format("{0} Lv. {1} - ID: {2} - {3}",
                 model.PlayerName, model.PlayerLevel, model.PlayerId, Utils.FormatDuration(model.ServerTime));
         }
     }
