4898230e1b04ae11bbe742a57a073a45d5604c00
k_y_1989 <dienhuynhphong@gmail.com>
2017-04-03 22:22:38 +0700

    thêm sở nghiên cứu

---
+++ k8asd/Research/ResearchCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace k8asd {
+    public static class ResearchCommand
+    {
+        /// <summary>
+        /// Lấy danh sách các kỹ năng.
+        /// </summary>
+        public static async Task<Packet> GetListResearchAsync(this IPacketWriter writer) {
+            return await writer.SendCommandAsync("63601");
+        }
+
+        public static async Task<Packet> ChangeResearchAsync(this IPacketWriter writer, int department, int type)
+        {
+            return await writer.SendCommandAsync("63603", department.ToString(), type.ToString());
+        }
+
+        public static async Task<Packet> UpdateResearchAsync(this IPacketWriter writer, int department)
+        {
+            return await writer.SendCommandAsync("63604", department.ToString());
+        }
+    }
+}
