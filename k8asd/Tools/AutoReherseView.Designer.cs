00aa697f26871e67fe12bae03d3511fa1318a306
k_y_1989 <dienhuynhphong@gmail.com>
2017-04-06 21:55:27 +0700

    cập nhật tập trận

---
+++ k8asd/Tools/AutoReherseView.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace k8asd {
+    partial class AutoReherseView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.nameColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.levelColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.rankColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.playerList = new BrightIdeasSoftware.ObjectListView();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbState = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.playerList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.AspectName = "";
+            this.nameColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nameColumn.MaximumWidth = 150;
+            this.nameColumn.MinimumWidth = 150;
+            this.nameColumn.Text = "Người chơi";
+            this.nameColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nameColumn.Width = 150;
+            // 
+            // levelColumn
+            // 
+            this.levelColumn.AspectName = "";
+            this.levelColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.levelColumn.MaximumWidth = 40;
+            this.levelColumn.MinimumWidth = 40;
+            this.levelColumn.Text = "Cấp";
+            this.levelColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.levelColumn.Width = 40;
+            // 
+            // rankColumn
+            // 
+            this.rankColumn.AspectName = "";
+            this.rankColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.rankColumn.MaximumWidth = 60;
+            this.rankColumn.MinimumWidth = 60;
+            this.rankColumn.Text = "Hạng";
+            this.rankColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // playerList
+            // 
+            this.playerList.AllColumns.Add(this.rankColumn);
+            this.playerList.AllColumns.Add(this.nameColumn);
+            this.playerList.AllColumns.Add(this.levelColumn);
+            this.playerList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.playerList.CellEditUseWholeCell = false;
+            this.playerList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.rankColumn,
+            this.nameColumn,
+            this.levelColumn});
+            this.playerList.Cursor = System.Windows.Forms.Cursors.Default;
+            this.playerList.FullRowSelect = true;
+            this.playerList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.playerList.Location = new System.Drawing.Point(10, 80);
+            this.playerList.MultiSelect = false;
+            this.playerList.Name = "playerList";
+            this.playerList.SelectColumnsOnRightClick = false;
+            this.playerList.SelectColumnsOnRightClickBehaviour = BrightIdeasSoftware.ObjectListView.ColumnSelectBehaviour.None;
+            this.playerList.ShowGroups = false;
+            this.playerList.Size = new System.Drawing.Size(480, 370);
+            this.playerList.TabIndex = 30;
+            this.playerList.UseCompatibleStateImageBehavior = false;
+            this.playerList.View = System.Windows.Forms.View.Details;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(175, 10);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(114, 30);
+            this.refreshButton.TabIndex = 31;
+            this.refreshButton.Text = "Làm mới danh sách";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Số trang";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(67, 16);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 35;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Nhân vật";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 37;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 30);
+            this.button1.TabIndex = 38;
+            this.button1.Text = "Tập trận";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbState
+            // 
+            this.lbState.AutoSize = true;
+            this.lbState.Location = new System.Drawing.Point(295, 53);
+            this.lbState.Name = "lbState";
+            this.lbState.Size = new System.Drawing.Size(0, 13);
+            this.lbState.TabIndex = 39;
+            // 
+            // AutoReherseView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 461);
+            this.Controls.Add(this.lbState);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.playerList);
+            this.Name = "AutoReherseView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoArenaView";
+            ((System.ComponentModel.ISupportInitialize)(this.playerList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.ObjectListView playerList;
+        private System.Windows.Forms.Button refreshButton;
+        private BrightIdeasSoftware.OLVColumn rankColumn;
+        private BrightIdeasSoftware.OLVColumn nameColumn;
+        private BrightIdeasSoftware.OLVColumn levelColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbState;
+    }
+}
---
+++ k8asd/Tools/AutoReherseView.cs
@@ -0,0 +1,176 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace k8asd {
+    public partial class AutoReherseView : Form {
+        private int LineLimit = 500;
+
+        /// <summary>
+        /// Ánh xạ từ ID sang Client (để gửi/nhận gói tin).
+        /// </summary>
+        private Dictionary<int, IClient> clients;
+
+        /// <summary>
+        /// Ánh xạ từ ID sang thông tin võ đài (64005).
+        /// </summary>
+        private Dictionary<int, ReherseInfo> infos;
+
+        /// <summary>
+        /// Danh sách ID của người chơi để tự động đánh võ đài.
+        /// </summary>
+        private List<int> playerIds;
+
+        /// <summary>
+        /// Có đang làm mới thông tin võ đài không?
+        /// </summary>
+        private bool isRefreshing;
+
+        /// <summary>
+        /// Có đang khiêu chiên võ đài không?
+        /// </summary>
+        private bool isDueling;
+
+
+        public AutoReherseView() {
+            InitializeComponent();
+
+            clients = new Dictionary<int, IClient>();
+            infos = new Dictionary<int, ReherseInfo>();
+            playerIds = new List<int>();
+
+            isRefreshing = false;
+            isDueling = false;
+
+            rankColumn.AspectGetter = obj => {
+                var info = infos[(int) obj];
+                return String.Format("{0}", info.top);
+            };
+           
+            nameColumn.AspectGetter = obj => {
+                var info = infos[(int) obj];
+                return info.playername;
+            };
+            levelColumn.AspectGetter = obj => {
+                var info = infos[(int) obj];
+                return info.playerlv;
+            };
+
+        }
+
+        private void RemovePlayer(int playerId) {
+            playerIds.Remove(playerId);
+            clients.Remove(playerId);
+            infos.Remove(playerId);
+            playerList.SetObjects(playerIds);
+        }
+
+        private async void refreshButton_Click(object sender, EventArgs e) {
+            await RefreshPlayersAsync();
+        }
+
+        private List<IClient> FindConnectedClients() {
+            var clients = ClientManager.Instance.Clients;
+            var connectedClients = new List<IClient>();
+            foreach (var client in clients) {
+                if (client.ConnectionStatus == ConnectionStatus.Connected) {
+                    connectedClients.Add(client);
+                }
+            }
+            return connectedClients;
+        }
+
+        /// <summary>
+        /// Cập nhật võ đài tất cả các tài khoản đang kết nối.
+        /// </summary>
+        private async Task<bool> RefreshPlayersAsync() {
+            if (isRefreshing) {
+                return false;
+            }
+            if (isDueling) {
+                return false;
+            }
+            isRefreshing = true;
+
+            clients.Clear();
+            infos.Clear();
+            playerIds.Clear();
+            playerList.Items.Clear();
+
+            //tim trang dau tien
+            //tim client co ket noi dau tien
+            var listClient = ClientManager.Instance.Clients;
+            Packet packet = new Packet();
+            foreach (var client in listClient)
+            {
+                if (client.ConnectionStatus == ConnectionStatus.Connected)
+                {
+                    packet = await client.RefreshListMemberAsync((int)numericUpDown1.Value);
+                    break;
+                }
+            }
+            List<ReherseInfo> list = Parse32101(packet);
+
+            foreach (var client in list)
+            {
+                var playerId = client.playerid;
+                playerIds.Add(playerId);
+               // clients.Add(playerId, client);
+                infos.Add(playerId, client);
+
+                playerList.SetObjects(playerIds, true);
+            }
+
+            isRefreshing = false;
+            return true;
+        }
+
+        private List<ReherseInfo> Parse32101(Packet packet)
+        {
+            List<ReherseInfo> list = new List<ReherseInfo>();
+            var token = JToken.Parse(packet.Message);
+            JArray array = (JArray)token["legion_memberdto"];
+
+            foreach (var item in array)
+            {
+                list.Add(ReherseInfo.Parse(item));
+            }
+            return list;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (!"".Equals(this.textBox1.Text))
+            {
+                int playerid = 0;
+                foreach (var playerId in playerIds)
+                {
+                    var info = infos[playerId];
+                    if (this.textBox1.Text.Equals(info.playername))
+                    {
+                        playerid = info.playerid;
+                        break;
+                    }
+                }
+                if (playerid != 0)
+                {
+                    var clients = ClientManager.Instance.Clients;
+                    this.lbState.Text = "Đang tập trận";
+                    foreach (var client in clients)
+                    {
+                        if (client.ConnectionStatus == ConnectionStatus.Connected)
+                        {
+                            await client.ReherseAsync(playerid);
+                        }
+                    }
+                    this.lbState.Text = "Tập trận xong";
+                }
+            }
+        }       
+    }
+}
