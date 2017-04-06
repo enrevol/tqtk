bcc565a007f79705e0b253eabc2804a17c810676
k_y_1989 <dienhuynhphong@gmail.com>
2017-04-06 21:52:04 +0700

    cập nhật tập trận

---
+++ k8asd/Reherse/ReherseCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k8asd
+{
+    public static class ReherseCommand
+    {
+        public static async Task<Packet> RefreshListMemberAsync(this IPacketWriter writer, int page)
+        {
+            return await writer.SendCommandAsync("32101", (page-1).ToString(), "12", " ");
+        }
+
+        public static async Task<Packet> ReherseAsync(this IPacketWriter writer, int playerid)
+        {
+            return await writer.SendCommandAsync("32132", playerid.ToString());
+        }
+    }
+}
---
+++ k8asd/Reherse/ReherseInfo.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k8asd
+{
+    public class ReherseInfo
+    {
+        public int playerid;
+        public string playername;
+        public int playerlv; //so sao
+        public int top;
+
+        public static ReherseInfo Parse(JToken token)
+        {
+            var result = new ReherseInfo();
+            result.playerid = (int)token["playerid"];
+            result.playername = token["playername"].ToString();
+            result.playerlv = (int)token["playerlv"];
+            result.top = (int)token["top"];
+
+            return result;
+        }
+    }
+}
