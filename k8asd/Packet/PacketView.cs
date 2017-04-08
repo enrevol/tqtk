209eb402884ea1ca7377fc56a75fff327f37142a
Zinge <enrevol@gmail.com>
2017-04-08 22:08:37 +0700

    [Packet] Clear reply message before sending commands.

--- k8asd/Packet/PacketView.cs
+++ k8asd/Packet/PacketView.cs
@@ -23,21 +23,19 @@ namespace k8asd {
         }
 
         private async void sendButton_Click(object sender, System.EventArgs e) {
-            sendButton.Enabled = false;
             var commandId = commandInput.Text;
             var param1 = param1Input.Text;
             var param2 = param2Input.Text;
             var param3 = param3Input.Text;
             var param4 = param4Input.Text;
             await SendCommand(commandId, param1, param2, param3, param4);
-            sendButton.Enabled = true;
         }
 
         private async Task SendCommand(string commandId, params string[] parameters) {
             if (commandId.Length == 0) {
                 return;
             }
-
+            replyBox.Clear();
             var nonEmptyParameters = parameters.Where(p => p.Length > 0).ToArray();
             var packet = await packetWriter.SendCommandAsync(commandId, nonEmptyParameters);
             if (packet == null) {
