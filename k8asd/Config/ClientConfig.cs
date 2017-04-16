38b62f910927364048d3f0c47f5cc19d65182833
Zinge <enrevol@gmail.com>
2017-04-17 00:24:02 +0700

    Remove client config custom comparator.

--- k8asd/Config/ClientConfig.cs
+++ k8asd/Config/ClientConfig.cs
@@ -35,17 +35,5 @@ namespace k8asd {
             get { return Get(PasswordKey); }
             set { Put(PasswordKey, value); }
         }
-
-        public override bool Equals(object obj) {
-            var item = obj as ClientConfig;
-            if (item == null) {
-                return false;
-            }
-            return ServerId == item.ServerId && Username == item.Username;
-        }
-
-        public override int GetHashCode() {
-            return (ServerId.ToString() + Username).GetHashCode();
-        }
     }
 }
