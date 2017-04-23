01373e473b5ec767ee0bbc22be01ab4d53702326
Zinge <enrevol@gmail.com>
2017-04-23 15:56:39 +0700

    Predict impose tasks.

--- k8asd/Task/ImposeTaskHelper.cs
+++ k8asd/Task/ImposeTaskHelper.cs
@@ -1,14 +1,34 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace k8asd {
-    class ImposeTaskHelper : ITaskHelper {
-        public async Task<TaskResult> Do(IPacketWriter writer, int times) {
-            var imposeInfo = await writer.RefreshImposeAsync();
-            if (imposeInfo == null) {
-                return TaskResult.LostConnection;
+    public class ImposeTaskHelper : ITaskHelper {
+        private IPacketWriter writer;
+        private IInfoModel info;
+        private ImposeInfo impose;
+
+        public ImposeTaskHelper(IPacketWriter writer, IInfoModel info, ImposeInfo impose) {
+            this.writer = writer;
+            this.info = info;
+            this.impose = impose;
+        }
+
+        public double PredictDifficulty(int times) {
+            if (impose.ImposeNum < times) {
+                return 1000;
+            }
+            var silverEachImpose = impose.Copper;
+            if (info.Silver + silverEachImpose * times > info.MaxSilver) {
+                // Tràn bạc.
+                return 100;
             }
 
-            var remainImposes = imposeInfo.ImposeNum;
+            // Tăng độ khó vì đóng băng lâu.
+            return times;
+        }
+
+        public async Task<TaskResult> Do(int times) {
+            var remainImposes = impose.ImposeNum;
             if (remainImposes < times) {
                 return TaskResult.CanNotBeDone;
             }
